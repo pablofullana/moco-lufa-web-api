ef701a575e5653291f17c448812d6f5d46a4453e
pablofullana <pablofullana@gmail.com>
2017-08-30 15:33:34 -0300

    Change HW_CDC_BULK_OUT_SIZE and HW_CDC_BULK_IN_SIZE from 8 to 16.

--- lib/LUFA-100807/Projects/mocolufa-master/dualMoco.h
+++ lib/LUFA-100807/Projects/mocolufa-master/dualMoco.h
@@ -3,7 +3,7 @@
      Copyright (C) 2013 by morecat_lab
 
      2013/09/22
-              
+
      http://morecatlab.akiba.coocan.jp/
 
      based on LUFA-100807
@@ -11,7 +11,7 @@
 /*
              LUFA Library
      Copyright (C) Dean Camera, 2010.
-              
+
   dean [at] fourwalledcubicle [dot] com
       www.fourwalledcubicle.com
 */
@@ -19,13 +19,13 @@
 /*
   Copyright 2010  Dean Camera (dean [at] fourwalledcubicle [dot] com)
 
-  Permission to use, copy, modify, distribute, and sell this 
+  Permission to use, copy, modify, distribute, and sell this
   software and its documentation for any purpose is hereby granted
-  without fee, provided that the above copyright notice appear in 
+  without fee, provided that the above copyright notice appear in
   all copies and that both that the copyright notice and this
-  permission notice and warranty disclaimer appear in supporting 
-  documentation, and that the name of the author not be used in 
-  advertising or publicity pertaining to distribution of the 
+  permission notice and warranty disclaimer appear in supporting
+  documentation, and that the name of the author not be used in
+  advertising or publicity pertaining to distribution of the
   software without specific, written prior permission.
 
   The author disclaim all warranties with regard to this
@@ -61,23 +61,23 @@
 		#include <LUFA/Drivers/Peripheral/Serial.h>
 		#include <LUFA/Drivers/USB/USB.h>
 		#include <LUFA/Drivers/USB/Class/CDC.h>
-		
+
 	/* Macros: */
 		/** LED mask for the library LED driver, to indicate TX activity. */
 		#define LEDMASK_TX               LEDS_LED1
 
 		/** LED mask for the library LED driver, to indicate RX activity. */
 		#define LEDMASK_RX               LEDS_LED2
-		
+
 		/** LED mask for the library LED driver, to indicate that an error has occurred in the USB interface. */
 		#define LEDMASK_ERROR            (LEDS_LED1 | LEDS_LED2)
-		
+
 		/** LED mask for the library LED driver, to indicate that the USB interface is busy. */
-		#define LEDMASK_BUSY             (LEDS_LED1 | LEDS_LED2)		
-		
+		#define LEDMASK_BUSY             (LEDS_LED1 | LEDS_LED2)
+
 		typedef uint8_t uchar;
-		#define HW_CDC_BULK_OUT_SIZE     8
-		#define HW_CDC_BULK_IN_SIZE      8
+		#define HW_CDC_BULK_OUT_SIZE     16
+		#define HW_CDC_BULK_IN_SIZE      16
 
 		#define	TRUE			1
 		#define	FALSE			0
@@ -91,9 +91,9 @@
 		void EVENT_USB_Device_Disconnect(void);
 		void EVENT_USB_Device_ConfigurationChanged(void);
 		void EVENT_USB_Device_UnhandledControlRequest(void);
-		
+
 		void EVENT_CDC_Device_LineEncodingChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo);
-		void EVENT_CDC_Device_ControLineStateChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo);		
+		void EVENT_CDC_Device_ControLineStateChanged(USB_ClassInfo_CDC_Device_t* const CDCInterfaceInfo);
 
 		uchar parseSerialMidiMessage(uchar);
 		void parseUSBMidiMessage(uchar *, uchar);
