a63200b5b24e7bf61cb91ab4a6c84208b07e4664
pablofullana <pablofullana@gmail.com>
2019-11-21 10:54:26 -0300

    Setup CORS origin whitelisting

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,10 @@ module MocoLufaWebApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        origins ENV['WHITELISTED_ORIGINS'].split
+        resource '*',
+          headers: :any,
+          methods: :any
       end
     end
   end
